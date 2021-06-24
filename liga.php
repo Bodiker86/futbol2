daa2f124d927207bceccf3a5335b067c58c0347b
Bodiker <Bodiker86@gmail.com>
2021-06-24 16:14:59 +0200

    HTML

---
+++ liga.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>piłka nożna</title>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+
+<body>
+    <header>
+        <h3>Reprezentacja polski w piłce nożnej</h3>
+        <img src="obraz1.jpg" alt="reprezentacja">
+    </header>
+    <div id="lewy">
+        <form action="liga.php" method="post">
+            <select>
+                <option value="">Bramkarze</option>
+                <option value="">Obrońcy</option>
+                <option value="">Pomocnicy</option>
+                <option value="">Napastnicy</option>
+            </select>
+            <button type="submit">Zobacz</button>
+            <img src="zad2.png" alt="piłka">
+            <p>Autor: 86123019619</p>
+        </form>
+        
+    </div>
+    <div id="prawy">
+        <ol>
+            <li>
+                <?php
+
+
+                ?>
+            </li>
+        </ol>
+    </div>
+    <main>
+        <h3>Liga mistrów</h3>
+    </main>
+    <div id="liga">
+        <?php
+
+        ?>
+    </div>
+
+</body>
+
+</html>
